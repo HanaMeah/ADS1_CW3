b1fb8c853ed3173063feb36452cfb53efa4cd404
HanaMeah <148878773+HanaMeah@users.noreply.github.com>
2024-01-25 21:42:06 +0000

    Add files via upload

--- ADS1 CW3 16048117.py
+++ ADS1 CW3 16048117.py
@@ -36,9 +36,26 @@ def reading_my_data(datafilename):
 
     # Transposing my dataframe
     df_transposed = df_orig.transpose()
+    
+    #clean transposed data
     df_transposed.columns = df_transposed.iloc[0]
     df_transposed = df_transposed.iloc[1:]
     df_transposed = df_transposed.apply(pd.to_numeric)
     df_transposed = df_transposed.dropna()
+    
+    # reset index and insert a column for 'Year'
+    df_transposed.reset_index(drop=True, inplace=True)
+    df_transposed.insert(0, 'Year',
+                      ['1990', '1991', '1992','1993', '1994', '1995', '1996',
+                        '1997', '1998', '1999', '2000', '2001', '2002', '2003',
+                        '2004', '2005', '2006', '2007', '2008', '2009', '2010',
+                        '2011', '2012', '2013', '2014', '2015', '2016', '2017',
+                        '2018', '2019', '2020'])
 
     return df_transposed, df_orig
+
+df_CO2_transposed, df_CO2_orig = reading_my_data("CO2_emmisions.csv")
+
+df_CO2_transposed.head(10)
+
+
